1ded95b9bd0c155b749224eef9622bc77ea4df5e
nikhiljayant <njayant86@gmail.com>
2025-09-17 23:54:16 +0530

    feat: implement CinemaSeatBooking component with default props

--- src/components/CinemaSeatBooking.jsx
+++ src/components/CinemaSeatBooking.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+const CinemaSeatBooking = ({
+  layout = {
+    rows: 8,
+    seatsPerRow: 12,
+    aislePosition: 5,
+  },
+  seatTypes = {
+    regular: { name: "Regular", price: 150, rows: [0, 1, 2] },
+    premium: { name: "Premium", price: 250, rows: [3, 4, 5] },
+    vip: { name: "VIP", price: 150, rows: [6, 7] },
+  },
+  bookedSeats = [],
+  currency = "INR",
+  onBookingComplete = () => {},
+  title = "Cinema Seat Booking",
+  subTitle = "Select your seats",
+}) => {
+  return <div></div>;
+};
 
-const CinemaSeatBooking = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default CinemaSeatBooking
+export default CinemaSeatBooking;
